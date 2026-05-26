5a49631b6b62b45a475af28e5aaf052338050414
Thiago-TP <thiagotomasdepaula@gmail.com>
2026-05-25 22:25:11 -0300

    apresentação ihuuuuuu

--- trabalho_6/src/main.py
+++ trabalho_6/src/main.py
@@ -53,6 +53,7 @@ def plota_correlacao_da_classe(csv: str, X_train_classe: pd.DataFrame, classe: s
     cmap = sns.diverging_palette(220, 10, as_cmap=True)
     figsize = (X_train_classe.shape[1], X_train_classe.shape[1])
 
+    sns.set_theme(font_scale=1.0)
     plt.figure(figsize=figsize)
     sns.heatmap(
         X_train_classe.corr(),
@@ -73,29 +74,91 @@ def plota_correlacao_da_classe(csv: str, X_train_classe: pd.DataFrame, classe: s
 def plota_matriz_de_confusao(csv: str, matriz_confusao: np.ndarray, classificador: str, out_dir: str):
     nome_csv = path.basename(csv)
     nome_matriz = f"matriz_confusao_{classificador}_{nome_csv.replace('.csv', '.pdf')}"
-
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    sns.set_theme(font_scale=1.5)
     plt.figure(figsize=(6, 5))
     sns.heatmap(
-        matriz_confusao,
+        matriz_confusao * 100,  # converte para porcentagem
+        vmin=0,
+        vmax=100,
         annot=True,
-        fmt="d",
-        cmap="Blues",
+        fmt=".1f",
+        cmap=cmap,
         cbar=False,
         square=True,
     )
     plt.xlabel("Predito")
     plt.ylabel("Verdadeiro")
-    plt.title(f"Matriz de Confusão - {classificador} - {nome_csv}")
+    # plt.title(f"Matriz de Confusão - {classificador} - {nome_csv}")
     plt.savefig(path.join(out_dir, nome_matriz), bbox_inches="tight")
     plt.close()
+
+
+def tabela_acuracias(resultados: dict, out_dir: str):
+    df_resultados = pd.DataFrame(resultados).T
+    # Ordena por nome do CSV (conjunto de imagens)
+    df_resultados = df_resultados.sort_index() * 100  # converte para porcentagem
+    # Renomeia linhas para nomes mais legíveis (opcional)
+    def renomeia(nome: str) -> str:
+        """
+        Renomeia string no formato 
+        'trabalho_X_features_bins_Y-PARCIAL' para 
+        'Trab. X, Y bins (parcial)'
+        """
+        import re
+        match = re.match(r'trabalho_(\d+)_features_bins_(\d+)(-PARCIAL)?', nome)
+        if match:
+            x = match.group(1)
+            y = match.group(2)
+            parcial = match.group(3)
+            resultado = f"Trab. {x}, {y} bins"
+            if parcial:
+                resultado += " (parcial)"
+            return resultado
+        return nome
+    df_resultados.index = df_resultados.index.map(renomeia)
+    nome_tabela = "tabela_acuracias.pdf"
+    sns.set_theme(font_scale=1.5)
+    _, axs = plt.subplots(nrows=2, ncols=1, figsize=(8, 6))
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+    vmin = df_resultados.min(axis=None)
+    vmax = df_resultados.max(axis=None)
+    kwargs = dict(
+        annot=True,
+        fmt=".2f",
+        cmap=cmap,
+        cbar=False,
+        linewidths=0.5,
+        vmin=vmin,
+        vmax=vmax
+    )
+    for parcial, valores in df_resultados.groupby(df_resultados.index.str.contains("Trab. 4")):
+        if parcial:  # parcial
+            sns.heatmap(
+                ax=axs[0],
+                data=valores,  # converte para porcentagem
+                xticklabels=False,
+                **kwargs
+            )
+            axs[0].set_title("Acurácia dos Classificadores (%)")
+        else:  # completo
+            sns.heatmap(
+                ax=axs[1],
+                data=valores,  # converte para porcentagem
+                **kwargs
+            )
+
+    plt.savefig(path.join(out_dir, nome_tabela), bbox_inches="tight")
+    plt.close()
+    print(f"    Tabela de acurácias salva como {nome_tabela}.")
 
 
 def main(out_dir: str = "trabalho_6/resultados"):
     # 5.1
     # CSVs são gerados executando o script monta_csvs.py, que extrai as features e escreve os arquivos CSV.
     # Assumindo que isso já foi feito, o item 5.1 está resolvido.
-
-    csvs = glob("trabalho_6/csvs/trabalho_*_features_bins_*-PARCIAL.csv")
+    acuracias = {}
+    csvs = glob("trabalho_6/csvs/trabalho_*_features_bins_*.csv")
     for csv in csvs:
         print(f"Avaliando desempenho usando features do arquivo {csv}...")
 
@@ -117,12 +180,12 @@ def main(out_dir: str = "trabalho_6/resultados"):
         features_correlacionadas = set()
         for classe in y_train.sort_values().unique():
             X_train_classe = X_train[y_train == classe]
+            constantes_classe = []
             for coluna in X_train_classe.columns:
                 if X_train_classe[coluna].nunique() <= 1:
                     features_constantes.add(coluna)
+                    constantes_classe.append(coluna)
             corr_matrix = X_train_classe.drop(columns=features_constantes).corr(numeric_only=True)
-            print(f"    Correlação para classe {classe}:")
-            print(corr_matrix.to_string())
             upper_tri = corr_matrix.where(
                 np.triu(np.ones(corr_matrix.shape), k=1).astype(bool)
             )
@@ -133,6 +196,17 @@ def main(out_dir: str = "trabalho_6/resultados"):
                 print(f"    Encontradas {len(features_classe)} features altamente correlacionadas.")
                 features_correlacionadas.update(features_classe)
                 print(f"    Features removidas por alta correlação: {features_classe}")
+            
+            # Plota matriz de correlação da classe apenas onde índices estão em corr_matrix
+            x = X_train_classe.copy()
+            x[features_classe]= np.nan
+            x[constantes_classe] = np.nan
+            plota_correlacao_da_classe(
+                csv, 
+                x.corr(numeric_only=True), 
+                classe, 
+                out_dir
+            )
 
         X_train = X_train.drop(columns=features_correlacionadas.union(features_constantes))
         X_test = X_test.drop(columns=features_correlacionadas.union(features_constantes))
@@ -155,13 +229,20 @@ def main(out_dir: str = "trabalho_6/resultados"):
         acuracia_qda = accuracy_score(y_test, qda.predict(X_test))
 
         # Resultados no teste: matriz de confusão
-        matriz_confusao_naive_bayes = confusion_matrix(y_test, nb.predict(X_test))
-        matriz_confusao_lda = confusion_matrix(y_test, lda.predict(X_test))
-        matriz_confusao_qda = confusion_matrix(y_test, qda.predict(X_test))
+        matriz_confusao_naive_bayes = confusion_matrix(y_test, nb.predict(X_test), normalize="true")
+        matriz_confusao_lda = confusion_matrix(y_test, lda.predict(X_test), normalize="true")
+        matriz_confusao_qda = confusion_matrix(y_test, qda.predict(X_test), normalize="true")
 
-        plota_matriz_de_confusao(csv, matriz_confusao_naive_bayes, "Naive_Bayes", out_dir)
+        plota_matriz_de_confusao(csv, matriz_confusao_naive_bayes, "Naïve Bayes", out_dir)
         plota_matriz_de_confusao(csv, matriz_confusao_lda, "LDA", out_dir)
         plota_matriz_de_confusao(csv, matriz_confusao_qda, "QDA", out_dir)
+
+        # Armazena os resultados para a tabela de acurácias
+        acuracias[path.basename(csv).replace(".csv", "")] = {
+            "Naïve Bayes": acuracia_naive_bayes,
+            "LDA": acuracia_lda,
+            "QDA": acuracia_qda,
+        }
 
         # Salva os resultados em um arquivo numpy comprimido
         nome_csv = path.basename(csv)
@@ -177,6 +258,8 @@ def main(out_dir: str = "trabalho_6/resultados"):
                 "matriz_confusao_qda": matriz_confusao_qda,
             },
         )
+    
+    tabela_acuracias(acuracias, out_dir)
 
 
 if __name__ == "__main__":
